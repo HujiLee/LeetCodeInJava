9f1175fe4040eba2e098a06d5a21c7ecd28ef808
hujimiya <hujimiya@outlook.com>
2017-09-03 02:00:40 +0800

    微调,让代码DRY一些

--- src/problems/T002/Main.java
+++ src/problems/T002/Main.java
@@ -28,16 +28,16 @@ class ListNode {
 }
 
 /**
- You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
-
- You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
- Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
- Output: 7 -> 0 -> 8
-
- 也就是把一个数字用链表倒序的形式存储
- 比如,180(0->8->1),0(0),12306(6->0->3->2->1)
- 然后利用这个链表结构求两个数的和
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * <p>
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * <p>
+ * 也就是把一个数字用链表倒序的形式存储
+ * 比如,180(0->8->1),0(0),12306(6->0->3->2->1)
+ * 然后利用这个链表结构求两个数的和
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
@@ -56,12 +56,6 @@ class Solution {
                     if (l2.next == null) {
                         l2.next = new ListNode(0);
                     }
-                    l1 = l1.next;
-                    l2 = l2.next;
-                    digitSum = (l1.val + l2.val) % 10;
-                    carry = (l1.val + l2.val) / 10;
-                    p.next = new ListNode(digitSum);
-                    p = p.next;
                 }
             } else {
                 if (l1.next == null) {
@@ -70,14 +64,15 @@ class Solution {
                 if (l2.next == null) {
                     l2.next = new ListNode(0);
                 }
-                l1.next.val+=carry;
-                l1 = l1.next;
-                l2 = l2.next;
-                digitSum = (l1.val + l2.val) % 10;
-                carry = (l1.val + l2.val) / 10;
-                p.next = new ListNode(digitSum);
-                p = p.next;
+                l1.next.val += carry;
+
             }
+            l1 = l1.next;
+            l2 = l2.next;
+            digitSum = (l1.val + l2.val) % 10;
+            carry = (l1.val + l2.val) / 10;
+            p.next = new ListNode(digitSum);
+            p = p.next;
         }
     }
 }
