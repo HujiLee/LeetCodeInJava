8571326c34902ed175681db345eb468a32d6b2ed
hujimiya <hujimiya@outlook.com>
2017-09-03 01:22:33 +0800

    补充题目说明(简要)

--- src/problems/T19/Main.java
+++ src/problems/T19/Main.java
@@ -49,6 +49,13 @@ class Solution {
 
 /**
  * @anchor https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+ * Given a linked list, remove the nth node from the end of list and return its head.
+
+For example,
+
+Given linked list: 1->2->3->4->5, and n = 2.
+
+After removing the second node from the end, the linked list becomes 1->2->3->5.
  */
 public class Main {
     public static void main(String[] args) {
