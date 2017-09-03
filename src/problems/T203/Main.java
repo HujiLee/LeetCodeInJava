9169b1f241f2a99df25a65c59a8d257d6c5d7311
hujimiya <hujimiya@outlook.com>
2017-09-03 23:27:28 +0800

    有瑕疵,比如输入[1,2,6,3,4,5,6]和6,题目要求把所有的6去掉;

---
+++ src/problems/T203/Main.java
@@ -0,0 +1,74 @@
+package problems.T203;
+
+/**
+ * Created by Administrator on 2017/9/3 0003.
+ */
+public class Main {
+    public static void main(String[] args) {
+        {
+            ListNode head = new ListNode(9);
+            head.setNext(8).setNext(5).setNext(2).setNext(1).setNext(0);
+            System.out.println(new Solution().removeElements(head,9));
+        }
+        {
+            ListNode head = new ListNode(9);
+            head.setNext(8).setNext(5).setNext(2).setNext(1).setNext(0);
+            System.out.println(new Solution().removeElements(head,0));
+        }
+        {
+            ListNode head = new ListNode(9);
+            head.setNext(8).setNext(5).setNext(2).setNext(1).setNext(0);
+            System.out.println(new Solution().removeElements(head,5));
+        }
+
+
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    /**
+     * DEBUG ONLY
+     * @param nextVal
+     * @return next
+     */
+    ListNode setNext(int nextVal){
+        if(next!=null){
+            next.val = nextVal;
+        }else{
+            next = new ListNode(nextVal);
+        }
+        return next;
+    }
+
+    @Override
+    public String toString() {
+        if(next!=null){
+            return val+"->"+next.toString();
+        }else{
+            return val+"|";
+        }
+    }
+}
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head.val==val){
+            if(head.next!=null)return head.next;
+            else return null;
+        }
+        ListNode p = head;
+        while (p.next!=null){
+            if(p.next.val==val){
+                p.next = p.next.next;
+                break;
+            }
+            p = p.next;
+        }
+        return head;
+    }
+}
