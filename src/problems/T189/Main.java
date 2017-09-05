e61d41f6a78cb694070df7b5af586191ad382f50
hujimiya <hujimiya@outlook.com>
2017-09-05 13:38:32 +0800

    WA;Input:[1,2] 3 Output:[1,2] Expected:[2,1]

--- src/problems/T189/Main.java
+++ src/problems/T189/Main.java
@@ -3,9 +3,14 @@ package problems.T189;
 /**
  * Created by Administrator on 2017/9/5 0005.
  */
+
+/**
+ * @anchor https://leetcode.com/problems/rotate-array/description/
+ */
 public class Main {
     public static void main(String[] args) {
-
+        int[] ints = new int[]{-1};
+        new Solution().rotate(ints,2);
     }
 }
 
@@ -34,6 +39,7 @@ class Solution {
         }
     }
     public void rotate(int[] nums, int k) {
+        if(k>=nums.length)return;
         reverse(nums,0,nums.length-k-1);
         reverse(nums,nums.length-k,nums.length-1);
         reverse(nums,0,nums.length-1);
