87cec9971a1e09531efd72111a587a40afca32f0
hujimiya <hujimiya@outlook.com>
2017-12-22 16:00:10 +0800

    AC

--- src/problems/T200/Main.java
+++ src/problems/T200/Main.java
@@ -1,18 +1,39 @@
 package problems.T200;
 
 class Solution {
-    static class Node{
-        Node parent;
+
+    int count = 0;
+    char[][] grid;
+
+    private char get(int x, int y) {
+        if (x < 0||y<0) return '0';
+        else if(x>=grid.length) return '0';
+        else if(y>=grid[x].length) return '0';
+        else return grid[x][y];
+    }
+
+    private void set(int x, int y,int minus_no){
+        if(x<0||y<0||x>=grid.length||y>=grid[x].length) return;
+        grid[x][y] = (char)minus_no;
+    }
+    void mark(int x,int y,int minus_no){
+        set(x,y,minus_no);
+        if(get(x-1,y)=='1') mark(x-1,y,minus_no);
+        if(get(x+1,y)=='1') mark(x+1,y,minus_no);
+        if(get(x,y-1)=='1') mark(x,y-1,minus_no);
+        if(get(x,y+1)=='1') mark(x,y+1,minus_no);
     }
     public int numIslands(char[][] grid) {
-        Node[][] nodes  = new Node[grid.length][]
-        for(int i = 0;i<grid.length;i++){
-            nodes[i] = new Node[grid[i].length];
-            for(int j = 0;j<nodes[i].length;j++) {
-                nodes[i][j] = null;
+        this.grid = grid;
+        for(int x = 0;x<grid.length;x++){
+            for(int y = 0;y<grid[x].length;y++){
+                if(get(x,y)=='1'){
+                    mark(x,y,--count);
+                }
             }
         }
-        return -1;
+
+        return -count;
     }
 }
 
@@ -36,6 +57,18 @@ public class Main {
                             new char[]{'0', '0', '0', '0', '0'}
                     }
             );
+            System.out.println(answer);
+        }
+        {
+            int answer = new Solution().numIslands(
+                    new char[][]{
+                            new char[]{'1','1','0','0','0'},
+                            new char[]{'1','1','0','0','0'},
+                            new char[]{'0','0','1','0','0'},
+                            new char[]{'0','0','0','1','1'}
+                    }
+            );
+            System.out.println(answer);
         }
 
     }
