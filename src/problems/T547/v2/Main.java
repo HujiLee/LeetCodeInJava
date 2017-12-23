a875941522e57f8dbd3d986e868457a964468784
hujimiya@vis <hujimiya@outlook.com>
2017-12-23 19:33:01 +0800

    虽然勉强AC了,但实际上比起其他人的提交算得上很慢.

--- src/problems/T547/v2/Main.java
+++ src/problems/T547/v2/Main.java
@@ -1,6 +1,8 @@
 package problems.T547.v2;
 
 
+import java.util.Stack;
+
 class Solution {
     int count = 0;
     int[][] matrix;
@@ -9,7 +11,7 @@ class Solution {
         final int N = Matrix.length;
 
         for (int x = 0; x < N; x++) {
-            for (int y = 0; y < N; y++) {
+            for (int y = 0; y <= x; y++) {
                 if (Matrix[x][y] == 1) {
                     mark(x, y, --count);
                 }
@@ -19,49 +21,30 @@ class Solution {
         return -count;
     }
 
-    int get(int x,int y){
-        if(x<0||y<0||x>=matrix.length||y>=matrix[x].length) return 0;
-        return matrix[x][y];
-    }
-
     void mark(int x, int y, int minus_count) {
         if(x<0||y<0||x>=matrix.length||y>=matrix[x].length)return;
-        matrix[x][y] = minus_count;
-        {
-            int $y = y+1;
-            while ($y<matrix[x].length){
-                if(get(x,$y)==1){
-                    matrix[x][$y] = minus_count;
+        Stack<int[]> stack = new Stack<>();
+        int[] temp;
+        stack.push(new int[]{x,y});
+        while (stack.size()>0){
+            temp = stack.pop();
+            matrix[temp[0]][temp[1]] = minus_count;
+//            System.err.printf("[%s,%s] ",temp[0],temp[1]);
+            {
+                int $x = temp[0];
+                for(int $y=0;$y<=$x;$y++){
+                    if($y!=temp[1]&&matrix[$x][$y]==1){
+                        stack.push(new int[]{$x,$y});
+                    }
                 }
-                $y++;
             }
-        }
-        {
-            int $y = y-1;
-            while ($y>=0){
-                if(get(x,$y)==1){
-                    matrix[x][$y] = minus_count;
+            {
+                int $y = temp[1];
+                for(int $x = $y;$x<matrix[0].length;$x++){
+                    if($x!=temp[0]&&matrix[$x][$y]==1){
+                        stack.push(new int[]{$x,$y});
+                    }
                 }
-                $y--;
-            }
-        }
-        {
-            int $x = x+1;
-            while ($x<matrix.length){
-                if(get($x,y)==1){
-                    matrix[$x][y] = minus_count;
-                }
-                $x++;
-            }
-
-        }
-        {
-            int $x = x-1;
-            while ($x>=0){
-                if(get($x,y)==1){
-                    matrix[$x][y] = minus_count;
-                }
-                $x--;
             }
 
         }
@@ -72,6 +55,14 @@ class Solution {
 public class Main {
     public static void main(String[] args) {
         {
+            int answer = new Solution().findCircleNum(new int[][]{
+                    new int[]{1, 1, 0},
+                    new int[]{1, 1, 0},
+                    new int[]{0, 0, 1}
+            });
+            System.out.println(answer);//should be 2
+        }
+        {
             int[][] ints = new int[100][100];
             for(int i = 0;i<100;i++){
                 for(int j=0;j<100;j++){
@@ -79,7 +70,21 @@ public class Main {
                 }
             }
             int answer = new Solution().findCircleNum(ints);
-            System.out.println(answer);
+            System.out.println(answer);//should be 1
+        }
+        {
+            int[][] ints = new int[100][100];
+            for(int i = 0;i<100;i++){
+                for(int j=0;j<100;j++){
+                    if(j%2==i%2){
+                        ints[i][j] = 1;
+                    }else{
+                        ints[i][j] = 0;
+                    }
+                }
+            }
+            int answer = new Solution().findCircleNum(ints);
+            System.out.println(answer);//should be 2
         }
 
     }
---
+++ src/problems/T547/v2/README.md
@@ -0,0 +1,17 @@
+https://leetcode.com/submissions/detail/132995008/
+
+Submission Detail
+
+113 / 113 test cases passed.
+
+Status: Accepted
+
+Runtime: 449 ms
+
+Submitted: 0 minutes ago
+
+Your runtime beats 0.22 % of java submissions.
+***
+用栈重写v1.
+
+虽然勉强AC了,但实际上比起其他人的提交算得上很慢.
