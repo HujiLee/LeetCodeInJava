2230f5cb59e0e29914fe3918049eb43df2c7dffa
hujimiya <hujimiya@outlook.com>
2017-12-26 23:12:05 +0800

    comment

--- src/problems/T547/v7_union_find/Main.java
+++ src/problems/T547/v7_union_find/Main.java
@@ -14,6 +14,10 @@ class Solution {
             this.val = val;
         }
 
+        /**
+         *
+         * @return this所在集合的·代表·
+         */
         Element find(){
             if(parent==null){
                 return this;
@@ -22,6 +26,11 @@ class Solution {
             }
         }
 
+
+        /**
+         * 如果this与element在不同的集合中,就将这两个集合合并
+         * @param element 另一个元素
+         */
         void union(Element element){
             Element myroot = find();
             Element yourroot = element.find();
