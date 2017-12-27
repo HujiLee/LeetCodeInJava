2052469a3ca164cb50085ed9f027dafc26e99d34
hujimiya@vis <hujimiya@outlook.com>
2017-12-27 19:46:12 +0800

    AC.<算法设计技巧与分析>沙特M.H.Alsuwaiyel,第5章归纳法.5.7.寻找多数元素.

---
+++ src/problems/T169/v3/Main.java
@@ -0,0 +1,57 @@
+package problems.T169.v3;
+
+class Solution {
+    int[] nums;
+
+    public int majorityElement(int[] $nums) {
+        this.nums = $nums;
+        int possible_majority_element = candidate(0);
+        int count = 0;
+        for(int num:nums){
+            if(num==possible_majority_element){
+                count++;
+            }
+        }
+        if(count*2>nums.length){
+            return possible_majority_element;
+        }
+        return Integer.MIN_VALUE;
+    }
+
+    int candidate(int m) {
+        int j = m;
+        int c = nums[m];
+        int count = 1;
+        while (j < nums.length - 1 && count > 0) {
+            j++;
+            if (nums[j] == c) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        if (j == nums.length - 1) {
+            return c;
+        } else {
+            return candidate(j + 1);
+        }
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        {
+            int answer = new Solution().majorityElement(new int[]{
+                    1, 2, 2, 1,2,4,2,1,2
+            });
+            System.out.println(answer);//2
+        }
+        {
+            int answer = new Solution().majorityElement(new int[]{
+                    1, 2, 3, 2, 5, -2, 1, 2, 3, 2, 9, 2, 2, 4, 2, 2, 9, 2, 2, 2
+            });
+            System.out.println(answer);//2
+        }
+    }
+}
---
+++ src/problems/T169/v3/README.md
@@ -0,0 +1,20 @@
+https://leetcode.com/submissions/detail/133477074/
+
+Submission Detail
+
+44 / 44 test cases passed.
+
+Status: Accepted
+
+Runtime: 4 ms
+
+Submitted: 0 minutes ago
+
+Your runtime beats 39.16 % of java submissions.
+
+***
+<算法设计技巧与分析>沙特M.H.Alsuwaiyel,第5章归纳法.5.7.寻找多数元素.
+
+算法设计的依据:
+
+**在原有序列中去掉两个不同的元素之后,原有序列中的`多数元素`在新系列中仍然是`多数元素`**.
