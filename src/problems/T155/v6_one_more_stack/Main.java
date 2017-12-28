28252881ff3a41a66f2f9863f338ebf9c54668ec
hujimiya@vis <hujimiya@outlook.com>
2017-12-28 21:33:36 +0800

    AC

---
+++ src/problems/T155/v6_one_more_stack/Main.java
@@ -0,0 +1,89 @@
+package problems.T155.v6_one_more_stack;
+
+
+class MinStack {
+
+    private static class HStack {
+
+        private int array_size = 1000;
+        private int stack_size = 0;
+        private int[] array = new int[array_size];
+
+        public HStack() {
+
+        }
+
+        public void push(int x) {
+            if (!(stack_size < array_size)) {
+                int[] new_array = new int[array_size * 2];
+                for (int i = 0; i < array_size; i++) {
+                    new_array[i] = array[i];
+                }
+                array_size += array_size;
+                array = new_array;
+            }
+            array[stack_size++] = x;
+        }
+
+        public void pop() {
+            stack_size--;
+        }
+
+        public int top() {
+            return array[stack_size - 1];
+        }
+
+        public boolean empty() {
+            return stack_size == 0;
+        }
+    }
+
+    private HStack hStack = new HStack();
+    private HStack min_hstack = new HStack();
+
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        hStack.push(x);
+        if (min_hstack.empty()) {
+            min_hstack.push(x);
+        } else {
+            if (x <= min_hstack.top()) {
+                min_hstack.push(x);
+            }
+        }
+    }
+
+    public void pop() {
+        int x = hStack.top();
+        hStack.pop();
+        if (x == min_hstack.top()) {
+            min_hstack.pop();
+        }
+
+    }
+
+    public int top() {
+        return hStack.top();
+    }
+
+    public int getMin() {
+        return min_hstack.top();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(9);
+        minStack.push(-2);
+        minStack.push(-3);
+        minStack.push(0);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
+}
---
+++ src/problems/T155/v6_one_more_stack/README.md
@@ -0,0 +1,16 @@
+https://leetcode.com/submissions/detail/133626015/
+
+Submission Detail
+
+18 / 18 test cases passed.
+
+Status: Accepted
+
+Runtime: 113 ms
+
+Submitted: 0 minutes ago
+
+Your runtime beats 39.79 % of java submissions.
+
+***
+相当于v2的改进,使用自己实现的stack.相对于v2有进步.
