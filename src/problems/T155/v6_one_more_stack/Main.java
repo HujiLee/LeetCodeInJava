a54f3a77bcef571190ff334233c0ccc80fef8639
hujimiya@vis <hujimiya@outlook.com>
2017-12-28 22:16:15 +0800

    优化栈的初始容量

--- src/problems/T155/v6_one_more_stack/Main.java
+++ src/problems/T155/v6_one_more_stack/Main.java
@@ -3,6 +3,40 @@ package problems.T155.v6_one_more_stack;
 
 class MinStack {
 
+    private static class HStack {
+
+        private int array_size = 7500;
+        private int stack_size = 0;
+        private int[] array = new int[array_size];
+
+        public HStack() {
+
+        }
+
+        public void push(int x) {
+            if (!(stack_size < array_size)) {
+                int[] new_array = new int[array_size * 2];
+                for (int i = 0; i < array_size; i++) {
+                    new_array[i] = array[i];
+                }
+                array_size += array_size;
+                array = new_array;
+            }
+            array[stack_size++] = x;
+        }
+
+        public void pop() {
+            stack_size--;
+        }
+
+        public int top() {
+            return array[stack_size - 1];
+        }
+
+        public boolean empty() {
+            return stack_size == 0;
+        }
+    }
 
     private HStack hStack = new HStack();
     private HStack min_hstack = new HStack();
@@ -39,40 +73,6 @@ class MinStack {
         return min_hstack.top();
     }
 
-    class HStack {
-
-        private int array_size = 1000;
-        private int stack_size = 0;
-        private int[] array = new int[array_size];
-
-        public HStack() {
-
-        }
-
-        public void push(int x) {
-            if (!(stack_size < array_size)) {
-                int[] new_array = new int[array_size * 2];
-                for (int i = 0; i < array_size; i++) {
-                    new_array[i] = array[i];
-                }
-                array_size += array_size;
-                array = new_array;
-            }
-            array[stack_size++] = x;
-        }
-
-        public void pop() {
-            stack_size--;
-        }
-
-        public int top() {
-            return array[stack_size - 1];
-        }
-
-        public boolean empty() {
-            return stack_size == 0;
-        }
-    }
 }
 
 public class Main {
--- src/problems/T155/v6_one_more_stack/README.md
+++ src/problems/T155/v6_one_more_stack/README.md
@@ -20,6 +20,7 @@ Your runtime beats 39.79 % of java submissions.
 HStack栈的初始容量和速度的关系->
 
     10000->108ms
+    7500->108ms/121ms
     6000->111ms
     4000->105ms
     2000->110ms
@@ -32,16 +33,34 @@ HStack栈的初始容量和速度的关系->
     4000->122ms
     6000->113ms
     7000->112ms
+    7500->111ms/108ms
     8000->101ms/111ms/132ms
     9000->112ms
     10000->115ms
     
 ~~private~~ static `class`
+HStack栈的初始容量和速度的关系->
+    1000->118ms
+    2000->122ms
+    4000->103ms
+    8000->109ms
+    7500->122ms
+    10000->130ms
+    15000->114ms
+    
+***
+试图通过把栈的容量恰好算作7500来加快速度.
+https://leetcode.com/submissions/detail/133628711/
 
-1000->118ms
-2000->122ms
-4000->103ms
-8000->109ms
-10000->130ms
-15000->114ms
+Submission Detail
+
+18 / 18 test cases passed.
+
+Status: Accepted
+
+Runtime: 108 ms
+
+Submitted: 0 minutes ago
+
+Your runtime beats 58.47 % of java submissions.
 
